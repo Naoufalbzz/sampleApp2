d67d820561dba7c4004c5db05d827200e657d6a2
Naoufalbzz <100003965+Naoufalbzz@users.noreply.github.com>
2023-10-09 01:11:16 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -euo pipefail
+directory='tempdirrrrr'
+mkdir $directory
+mkdir $directory/templates
+mkdir $directory/static
 
-mkdir tempdirrrr
-mkdir tempdirrrr/templates
-mkdir tempdirrrr/static
+cp sample_app.py $directory/.
+cp -r templates/* $directory/templates/.
+cp -r static/* $directory/static/.
 
-cp sample_app.py tempdirrrr/.
-cp -r templates/* tempdirrrr/templates/.
-cp -r static/* tempdirrrr/static/.
-
-cat > tempdirrrr/Dockerfile << _EOF_
+cat > $directory/Dockerfile << _EOF_
 FROM python
 RUN pip install flask
 COPY  ./static /home/myapp/static/
@@ -19,7 +19,7 @@ EXPOSE 5050
 CMD python /home/myapp/sample_app.py
 _EOF_
 
-cd tempdirrrr || exit
+cd $directory || exit
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a 
